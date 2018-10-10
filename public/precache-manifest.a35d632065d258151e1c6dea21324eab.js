1e0099b98fb855cc6e1f647570b5848cca21db55
Lauren Cahill <laurencahill@live.com>
2018-10-10 12:21:28 -0400

    full crud on user model now complete

---
+++ public/precache-manifest.a35d632065d258151e1c6dea21324eab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "300cfd8dd7d485939ad5",
+    "url": "/static/js/main.300cfd8d.chunk.js"
+  },
+  {
+    "revision": "4cf2a95a8ee9e65153cd",
+    "url": "/static/js/1.4cf2a95a.chunk.js"
+  },
+  {
+    "revision": "300cfd8dd7d485939ad5",
+    "url": "/static/css/main.b39bc127.chunk.css"
+  },
+  {
+    "revision": "397c854f060603dff2e4d1f9307a92ed",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6508f8915e094263ed75e616c5238715.js"
+  "/precache-manifest.a35d632065d258151e1c6dea21324eab.js"
 );
 
 workbox.clientsClaim();
