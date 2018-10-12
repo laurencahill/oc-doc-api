8675653856eee43d95062145fd34bfa893ed088b
Lauren Cahill <laurencahill@live.com>
2018-10-12 00:21:18 -0400

    most styling complete

---
+++ public/precache-manifest.a985b59299884df8f9013fc364c54273.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b558dd4a52acd3c7b8f",
+    "url": "/static/js/main.5b558dd4.chunk.js"
+  },
+  {
+    "revision": "21a4f7a1188282de527b",
+    "url": "/static/js/1.21a4f7a1.chunk.js"
+  },
+  {
+    "revision": "5b558dd4a52acd3c7b8f",
+    "url": "/static/css/main.97721647.chunk.css"
+  },
+  {
+    "revision": "f2a797897f3d6f7c0667a9d21617eb6c",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a35d632065d258151e1c6dea21324eab.js"
+  "/precache-manifest.a985b59299884df8f9013fc364c54273.js"
 );
 
 workbox.clientsClaim();
